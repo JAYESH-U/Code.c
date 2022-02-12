bb3f24f6c1436a97081c5382a575bac6a364c401
JAYESH-U <uramanattijayesh@gmail.com>
2022-02-12 19:02:27 +0530

    nothing just renamed folder

--- c/programs/reversing_a_sting.c
+++ c/programs/reversing_a_sting.c
@@ -5,7 +5,7 @@ void main()
 {
     char a[50];
     int i,n=0;
-    printf("enter the string the you want to reverse : ");
+    printf("enter the string the you want to reverse :  ");
     gets(a);
     n=strlen(a);
     for(i=n-1;i>=0;i--)
