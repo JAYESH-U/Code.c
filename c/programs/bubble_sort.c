4d30e56312bfab7f739d83c09dd3039ebdbcc124
JAYESH-U <uramanattijayesh@gmail.com>
2022-02-22 22:21:09 +0530

    bubble sort

---
+++ c/programs/bubble_sort.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int n, a[20], i, j, temp;
+    printf("Enter n:");
+    scanf("%d", &n);
+    printf("Enter %d numbers:", n);
+    for(i=0; i<n; i++)
+        scanf("%d", &a[i]);
+    for(i=0; i<n-1; i++)
+        for(j=0; j<n-i-1; j++)
+            if(a[j] > a[j+1])
+            {
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+    printf("Sorted array is:");
+    for(i=0; i<n; i++)
+        printf("%d ", a[i]);
+}
