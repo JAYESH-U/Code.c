7bcb314e710bd43d11ba10875a8a2af0dedcb88b
JAYESH-U <uramanattijayesh@gmail.com>
2022-03-20 19:14:37 +0530

    modification

--- c/lab programs/standard_deviation.c
+++ c/lab programs/standard_deviation.c
@@ -9,12 +9,10 @@ void main()
     scanf("%d",&n);
     printf("enter the array elements : ");
     for(i=0;i<n;i++)
-        scanf("%d",&a[i]);
-    p=&a[0];
-    for(i=0;i<n;i++)
     {
+        scanf("%d",&a[i]);
+        p=&a[i];
         sum=sum+*p;
-        p++;
     }
     mean=sum/n;
     p=&a[0];
