bda1c93dabc5c0339f7057d5a178b9721523616e
JAYESH-U <uramanattijayesh@gmail.com>
2022-05-17 20:05:14 +0530

    hello

---
+++ c/programs/additive_persistance.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int n,i,r,p=0,s=0;
+    printf("enter a number : ");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        s=0;
+        while(n!=0)
+        {
+            r=n%10;
+            s=s+r;
+            n=n/10;
+        }
+        p++;
+        if(s>10)
+        {
+            n=s; 
+        }
+    }
+    printf("additive persistance is : %d",p);
+}
