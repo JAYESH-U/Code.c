aa168f0863e23aa2af317342212ce908879f8b34
Jayesh <uramanattijayesh@gmail.com>
2023-01-09 20:44:29 +0530

    TW5

--- lab programs/3rd Sem/TW5_BunaryTree.c
+++ lab programs/3rd Sem/TW5_BunaryTree.c
@@ -7,11 +7,12 @@ typedef struct
     int roll_no;
     char name[20];
     char branch[5];
+    char usn[10];
 } Student_info;
 
 void display_stud_info(Student_info *data)
 {
-    printf("Roll no:%d Name:%s Branch:%s \n", data->roll_no, data->name, data->branch);
+    printf("USN:%s Roll no:%d Name:%s Branch:%s \n", data->usn, data->roll_no, data->name, data->branch);
 }
 
 struct node
@@ -33,6 +34,7 @@ struct node *insert(struct node *root, Student_info *val)
             p->data->roll_no = val->roll_no;
             strcpy(p->data->name, val->name);
             strcpy(p->data->branch, val->branch);
+            strcpy(p->data->usn, strupr(val->usn));
         }
         p->L = 0;
         p->R = 0;
@@ -46,12 +48,12 @@ struct node *insert(struct node *root, Student_info *val)
     while (q != NULL)
     {
         r = q;
-        if (val->roll_no < q->data->roll_no)
+        if (strcmp(val->usn, q->data->usn) < 0)
             q = q->L;
         else
             q = q->R;
     }
-    if (val->roll_no < r->data->roll_no)
+    if (strcmp(val->usn, r->data->usn) < 0)
         r->L = p;
     else
         r->R = p;
@@ -71,22 +73,25 @@ int main()
 {
     Student_info data;
     struct node *root = 0;
-    
+
     data.roll_no = 25;
     strcpy(data.name, "Jayesh");
     strcpy(data.branch, "ISE");
+    strcpy(data.usn, "2gi21is025");
     root = insert(root, &data);
 
     data.roll_no = 31;
     strcpy(data.name, "Manoj");
     strcpy(data.branch, "ISE");
+    strcpy(data.usn, "2gi21is031");
     root = insert(root, &data);
 
     data.roll_no = 10;
     strcpy(data.name, "Arnav");
     strcpy(data.branch, "ISE");
+    strcpy(data.usn, "2gi21is010");
     root = insert(root, &data);
-    
+
     in_trav(root);
     return 0;
 }
