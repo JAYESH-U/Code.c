f2bdb0c23b046ea9d1b711255efd39e01d0b1d9c
Jayesh <uramanattijayesh@gmail.com>
2023-01-15 18:40:30 +0530

    dLL

--- lab programs/3rd Sem/TW4_doubleLinkedList.c
+++ lab programs/3rd Sem/TW4_doubleLinkedList.c
@@ -13,6 +13,7 @@ void insertFront(struct node *p, int x)
     t->left = NULL;
     t->right = p;
     first = t;
+    printf("Inserted %d at front.\n",x);
 }
 
 void insertRear(struct node *p, int x)
@@ -20,12 +21,25 @@ void insertRear(struct node *p, int x)
     struct node *t = (struct node *)malloc(sizeof(struct node));
     t->data = x;
     t->right = NULL;
-    while (p->right != NULL)
+    if(!p)
     {
-        p = p->right;
+        t->left = NULL;
+        first = t;
+        printf("List is null and Inserted %d at rear.\n",x);
+        return;
     }
+        
+    else 
+    {
+        while (p->right != NULL)
+        {
+            p = p->right;
+        }
+    }
+    
     p->right = t;
     t->left = p;
+    printf("Inserted %d at rear.\n",x);
 }
 
 void insertAtRight(struct node *p, int k, int x)
@@ -43,6 +57,7 @@ void insertAtRight(struct node *p, int k, int x)
     p->right = t;
     if (q)
         q->left = t;
+    printf("Inserted %d at right of %d.\n",x,k);
 }
 
 void insertAtLeft(struct node *p, int k, int x)
@@ -60,32 +75,38 @@ void insertAtLeft(struct node *p, int k, int x)
     p->left = t;
     if (q)
         q->right = t;
-    first = t;
+    else 
+        first = t;
+    printf("Inserted %d at left of %d.\n",x,k);
 }
 
 void display(struct node *p)
 {
-    printf("\n");
+    printf("List : ");
     while (p != NULL)
     {
         printf("%d ", p->data);
         p = p->right;
     }
+    printf("\n\n");
 }
 
 void main()
 {
-    insertFront(first, 10);
-    insertFront(first, 20);
+    
     insertFront(first, 30);
+    insertFront(first, 20);
+    insertFront(first, 10);
+    display(first);
 
-    insertRear(first, 40);
     insertRear(first, 50);
     insertRear(first, 60);
+    insertRear(first, 70);
+    display(first);
 
-    // insertAtRight(first, 60, 70);
+    insertAtRight(first, 70, 80);
 
-    insertAtLeft(first, 30, 70);
+    insertAtLeft(first, 50, 40);
 
     display(first);
 }
